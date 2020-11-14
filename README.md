a35fde3d20da030c4cb589822a3dbc8b30b5ae80
shreyaasridhar <shreyanju@gmail.com>
2020-11-14 02:43:46 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-i5ezzv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-i5ezzv)
---
+++ src/app/material-textfield/material-textfield.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-material-textfield',
+  templateUrl: './material-textfield.component.html',
+  styleUrls: ['./material-textfield.component.css']
+})
+export class MaterialTextfieldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
